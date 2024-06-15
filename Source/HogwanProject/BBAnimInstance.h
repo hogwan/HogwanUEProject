e0d387201fc388846355dfc4561f21c512f5d20e
김관호 김관호 <asdf36597@naver.com>
2024-06-15 14:45:19 +0900

    김관호 : 애니메이션 블랜드 생성

---
+++ Source/HogwanProject/BBAnimInstance.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BBAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOGWANPROJECT_API UBBAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	void NativeInitializeAnimation() override;
+	void NativeUpdateAnimation(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	class AHunter* Hunter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	class UCharacterMovementComponent* HunterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	FVector MoveVector;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bLookOn = false;
+};
